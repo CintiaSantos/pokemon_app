20ca6c5fc795f8c6228e3410e4f4a5218d96811f
Cintia Santos <37563871+CintiaSantos@users.noreply.github.com>
2022-05-02 18:25:37 -0400

    Show view shows req.parmas.name

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ server.get('/pokemon', (req, res) => {
     res.json(pokemon)
   })
 
+server.get('/pokemon/:name', (req, res) => {
+    let name = req.params.name
+    const myPokemon = pokemon.find(obj => obj.name === name)
+    res.send(`
+    <h2>${myPokemon.name}</h2>
+    <img src=${myPokemon.img}.jpg >
+    `)
+    // res.json(myPokemon)
+  })
+
 server.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
 })
